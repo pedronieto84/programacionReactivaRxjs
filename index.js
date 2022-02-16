1fbf196bfd59676a2bc99106526ef901ecdc110c
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 21:38:22 +0100

    Observables que no se autofinalizan.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const timer = Rx.Observable.timer(1000)
+const interval = Rx.Observable.interval(1000)
 
-timer
-.finally(()=> print('finally'))
-.subscribe(()=> print('subscribe'))
+// Este intervalo nunca se finaliza si lo dejamos así
+interval
+.finally(()=>print('finally'))
+.subscribe((r)=> print(r))
 
 function print(val) {
     let el = document.createElement('p')
