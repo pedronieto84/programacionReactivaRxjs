dc9defcae34e539a884b1dc732e1821ea6bc819e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 21:43:29 +0100

    Riesgos de Observables abiertos, MEMORY LEAKS.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const interval = Rx.Observable.interval(1000)
+const interval = Rx.Observable.interval(1)
 
-// Este intervalo nunca se finaliza si lo dejamos así
+// Los observables que no se autofinalizan deben gestionarse (como veremos más adelante), o no se debe abusar de ellos, porque sino puede pasar que el navegador se queda pillado por falta de memoria. Ejemplo a no hacer nunca
+
+for (let index = 0; index < 100; index++) {
+    interval.subscribe((r)=> print(r))
+    
+}
 interval
 .finally(()=>print('finally'))
 .subscribe((r)=> print(r))
