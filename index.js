861c38a9f1d95c6c3f474ad3e925f33221267018
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 20:32:34 +0100

    Observable subscrito a clicks

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 // Me aseguro de que he importado la libreria y la logueo
 console.log(Rx);
 
-// Creo un observable
-const observable = Rx.Observable.create( observer =>{
-    // Le voy pasando acciones con el método next
-    observer.next('hello')
-    // Le voy pasando acciones con el método next
-    observer.next('world')
+// Observable que reacciona a clicks
+const clicks = Rx.Observable.fromEvent(document, 'click')
+
+// Me subscribo al observable
+clicks.subscribe((click)=>{
+    console.log(click);
 })
 
-// Me subscribo al observable y como callback llamo a la función predefinida anteriormente
-observable.subscribe (val =>print(val))
-
-// Función que nos printea en pantalla los cambios en vez de estar mirando todo el rato al console.log
-
-function print(val) {
-    let el = document.createElement('p')
-    el.innerText = val
-    document.body.appendChild(el)
-}
