e85a6b8d3cbed2579be52c13ba2727f6c91b5322
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 20:44:42 +0100

    Stream de datos variables. Crear un observable con método .of

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const interval = Rx.Observable.interval(1000)
+const deTodo = Rx.Observable.of(
+    'string',
+    ['1', 2, '2'],
+    23,
+    true,
+    {nombre:'Juan', apellido: 'Pardo'}
+)
 
-interval.subscribe(int => print(new Date().getSeconds()))
+deTodo.subscribe(val => print(val))
+
+
 function print(val) {
     let el = document.createElement('p')
     el.innerText = val
     document.body.appendChild(el)
 }
-
