d46c7b6cbe8f26ad80b3fc03ac8da4b8f1ae5fa6
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 21:46:22 +0100

    Unsubscribe de una Susbscription.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const interval = Rx.Observable.interval(1)
+const interval = Rx.Observable.interval(1000)
 
 // Los observables que no se autofinalizan deben gestionarse (como veremos más adelante), o no se debe abusar de ellos, porque sino puede pasar que el navegador se queda pillado por falta de memoria. Ejemplo a no hacer nunca
 
-for (let index = 0; index < 100; index++) {
-    interval.subscribe((r)=> print(r))
-    
-}
-interval
+// Esto es una subscripción.
+const subscription = interval
 .finally(()=>print('finally'))
 .subscribe((r)=> print(r))
+
+// Pasados 5 segundos fuerzo la terminación con el método unsubscribe.
+
+setTimeout(()=>{
+    subscription.unsubscribe()
+}, 5000)
 
 function print(val) {
     let el = document.createElement('p')
