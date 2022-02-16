d5259a8f92245859432657098e2bc41aa2226f7a
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 22:24:13 +0100

    Subject.next

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const subject = Rx.Subject.of('hello')
+const subject = new Rx.Subject()
 
 const subA = subject.subscribe(val => print(`Sub A: ${val}`))
 const subB = subject.subscribe(val => print(`Sub B: ${val}`))
 
 // Hasta aquí nada nuevo
+
+subject.next('Hello')
+
+setTimeout(()=>{
+    subject.next('World')
+}, 1000)
 
 function print(val) {
     let el = document.createElement('p')
