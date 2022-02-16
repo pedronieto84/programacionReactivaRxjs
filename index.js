487b6cf3952f0dbccc23b8633f45875a6c93126d
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 21:49:19 +0100

    Operators: map

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const interval = Rx.Observable.interval(1000)
 
-// Los observables que no se autofinalizan deben gestionarse (como veremos más adelante), o no se debe abusar de ellos, porque sino puede pasar que el navegador se queda pillado por falta de memoria. Ejemplo a no hacer nunca
 
 // Esto es una subscripción.
 const subscription = interval
-.finally(()=>print('finally'))
+.map(num => num * 2) // El método map transforma el dato
 .subscribe((r)=> print(r))
 
-// Pasados 5 segundos fuerzo la terminación con el método unsubscribe.
 
-setTimeout(()=>{
-    subscription.unsubscribe()
-}, 5000)
 
 function print(val) {
     let el = document.createElement('p')
