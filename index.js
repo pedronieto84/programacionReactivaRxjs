c50c1ea3ca8371dfe864fed6c848f303f75828fe
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 21:20:39 +0100

    Hot Observable

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// Cold Observable
+// Hot Observable
 // El dato se crea cuando alguien se subscribe, se crea desde dentro
 // del observable.
 
+const x = Math.random()
 const cold = Rx.Observable.create( observer =>{
-    observer.next(Math.random())
+    observer.next(x)
 })
 
 cold.subscribe((res)=> print(res))
 cold.subscribe((res)=> print(res))
 
-// El resultado cambia porque el dato se ha creado desde dentro
+// El resultado no cambia porque el dato se ha creado desde fuera
 
 
 function print(val) {
